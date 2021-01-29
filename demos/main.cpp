728f15fedfc9166172c383f0b21b4e3e3ae4fda2
Skrywerbeer <liquidsquid1@gmail.com>
2021-01-29 13:27:55 +0200

    Added plugin import path config file.

--- demos/main.cpp
+++ demos/main.cpp
@@ -1,8 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "importpath.h"
+
 int main(int argc, char **argv) {
 	QGuiApplication app(argc, argv);
 	QQmlApplicationEngine engine(QUrl("qrc:/demoLoader.qml"));
+	engine.addImportPath(PLUGIN_PATH);
 	return app.exec();
 }
